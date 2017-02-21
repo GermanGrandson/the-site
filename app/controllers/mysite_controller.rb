d890604d529eed7f5bbccdbb292292940f5089be
German Grandson <ganieto1@gmail.com>
2017-02-21 00:17:18 -0500

    update mobile

--- app/controllers/mysite_controller.rb
+++ app/controllers/mysite_controller.rb
@@ -1,14 +1,18 @@
 class MysiteController < ApplicationController
 
+  # Render mobile or desktop depending on User-Agent for these actions.
+  before_filter :check_for_mobile, :only => [:homepage]
+
 def homepage
 
 end
 
 def message
-  name = params[:message][:name]
-  email = params[:message][:email]
-  message = params[:message][:text]
+  name = params[:name]
+  email = params[:email]
+  message = params[:message]
   SiteMailer.send_message(name, email, message).deliver
+  render nothing: true
 end
 
 
